3943e51b1970a60ff87ceca4962f7f3aa88fbf52
Ruslan Filonchuk <itfreshman@ukr.net>
2022-01-07 20:29:34 +0200

    added solution for task 2

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Profile from './profile/Profile';
+import Statistics from './statistics/Statistics';
 import user from './data/profile/user.json';
+import statisticalData from './data/statistics/statistical-data.json';
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <Statistics stats={statisticalData} />
     </>
   );
 };
---
+++ src/Components/statistics/Statistics.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
+
+const Statistics = ({ title, stats }) => {
+  return (
+    <section className={s.statistics}>
+      {title && <h2 className={s.title}>{title}</h2>}
+
+      <ul className={s.statList}>
+        {stats.map(stat => {
+          const backgroundColorLi = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+          return (
+            <li
+              key={stat.id}
+              className={s.item}
+              style={{ backgroundColor: `${backgroundColorLi}` }}
+            >
+              <span className={s.label}>{stat.label}</span>
+              <span className={s.percentage}>{stat.percentage}%</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }),
+  ),
+};
+export default Statistics;

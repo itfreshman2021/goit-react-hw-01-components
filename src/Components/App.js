67931eab764065c3ecec72ab58b66238d3907639
Ruslan Filonchuk <itfreshman@ukr.net>
2022-01-09 16:07:59 +0200

    added solution for task 4

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Profile from './profile/Profile';
 import Statistics from './statistics/Statistics';
 import FriendList from './friendlist/FriendList';
+import TransactionHistory from './transactionhistory/TransactionHistory';
 import user from './data/profile/user.json';
 import statisticalData from './data/statistics/statistical-data.json';
 import friends from './data/friendlist/friends.json';
+import transactions from './data/transactionhistory/transactions.json';
 
 const App = () => {
   return (
@@ -19,6 +21,7 @@ const App = () => {
       <Statistics title="Upload stats" stats={statisticalData} />
       <Statistics stats={statisticalData} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
---
+++ src/Components/transactionhistory/TransactionHistory.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './TransactionHistory.module.css';
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className={s.transactionHistory}>
+      <thead className={s.transactionHistoryThead}>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(item => (
+          <tr key={item.id} className={s.transactionHistoryTr}>
+            <td className={s.transactionHistoryTd}>{item.type}</td>
+            <td className={s.transactionHistoryTd}>{item.amount}</td>
+            <td className={s.transactionHistoryTd}>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+    }),
+  ),
+};
+export default TransactionHistory;
